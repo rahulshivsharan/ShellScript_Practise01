ad3104134a2f8acddd9d8edd377904dd5ad33ed5
rahulshivsharan <rahulshivsharan@gmail.com>
2020-09-25 00:39:32 +0530

    Added some tricks for tabed spacing, coloring and variable usage
    	1. How to add tabs spacing within an echo statements in the
    output string
    	2. How to assign variables and then use it in echo statement
    	3. How to apply colors to words inside the echo sub-string

--- practise01.sh
+++ practise01.sh
@@ -3,3 +3,14 @@
 printf "%-5s %-10s %-4s\n" No Name Mark
 printf "%-5s %-10s %-4.2f\n" 1 Rahul 56.78
 printf "%-5s %-10s %-4.2f\n" 2 Vimal 99.5
+
+printf "\n"
+
+echo -e "No\tName\tMark"
+echo -e "1\tMicheal\t22.5"
+echo -e "1\tNinad\t45.65"
+echo -e "1\tGagan\t67.78"
+
+myvariable01="Rahul"
+
+echo -e "This name \e[1;33m ${myvariable01} \e[1;0m is from variable assignment"
